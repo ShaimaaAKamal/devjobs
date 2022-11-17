2a47624ab0687682f98cefd745f649133b03724e
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-17 19:42:52 +0200

    inIT APP BASIC ROUTING

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from '../Layout/Layout';
+import Home from '../Home/Home'
+import NotFound from "../NotFound/NotFound";
 
 function App() {
+  const routes=createBrowserRouter([{
+    path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
+      {index:true,element:<Home/>}
+    ]
+  }])
   return (
     <div className="App">
-      <h2>App</h2>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
