16793ea6aa5326c0ff4ad7d221ec48363ac2b9fd
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-17 19:29:27 +0200

    install dependiences

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './Components/App/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
