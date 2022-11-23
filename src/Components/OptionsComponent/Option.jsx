974d040bfb74b4e43be6a753fab6c089e4387fe7
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-23 19:09:15 +0200

    update search nav with styling

--- src/Components/OptionsComponent/Option.jsx
+++ src/Components/OptionsComponent/Option.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Option() {
   return (
      <div className='parent'>
-        <div className='container bg-white option px-0'>
+        <div className='container bg-white option px-0 '>
             <div className="row g-0">
             <div className="col-md-4 col-lg-5  d-md-none">
               <div className="input-group ">
@@ -32,24 +32,24 @@ export default function Option() {
 
               </div>
               </div>
-              <div className="col-md-4 col-lg-3 border-end border-start d-none d-md-block">
+              <div className="col-md-4 col-lg-3 bordergray d-none d-md-block">
                     <div className="input-group ">
                         <span className="input-group-text ps-3 pe-1  rounded-0" id="basic-addon1">
                         <svg width="17" height="24" xmlns="http://www.w3.org/2000/svg">
                           <path d="M14.358 2.451A8.3 8.3 0 008.448 0a8.3 8.3 0 00-5.911 2.451c-2.922 2.925-3.285 8.427-.786 11.76l6.697 9.683 6.687-9.669c2.508-3.347 2.145-8.85-.777-11.774zm-5.833 8.894a3.057 3.057 0 01-3.051-3.054 3.057 3.057 0 013.05-3.055 3.057 3.057 0 013.052 3.055 3.057 3.057 0 01-3.051 3.054z" fill="#5964E0" fillRule="nonzero"/>
                         </svg>
                         </span>
-                        <input type="text" className="form-control rounded-0 form-control-lg" placeholder="Filter by location…" id='locationKey' aria-label="Filter Key" aria-describedby="basic-addon1"/>
+                        <input type="text" className="form-control ps-2 rounded-0 form-control-lg" placeholder="Filter by location…" id='locationKey' aria-label="Filter Key" aria-describedby="basic-addon1"/>
                     </div>
               </div>
               <div className="col-md-4 col-lg-4 d-none d-md-block">
                   <div className='px-3 d-flex justify-content-between align-items-center h-100 Filter'>
                         <div className="form-check">
                               <input className="form-check-input" type="checkbox" value="" id="checkFull"/>
-                              <label className="form-check-label d-none d-lg-inline-block" htmlFor="checkFull">
+                              <label className="form-check-label  d-none d-lg-inline-block" htmlFor="checkFull">
                               Full Time Only                    
                               </label>
-                              <label className="form-check-label d-none d-md-inline-block d-lg-none" htmlFor="checkFull">
+                              <label className="form-check-label  d-none d-md-inline-block d-lg-none" htmlFor="checkFull">
                               Full Only                    
                               </label>
                         </div>
