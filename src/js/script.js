cd2e53741746ef49df0247f9e48604960584743c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-23 19:08:57 +0200

    update changeMode function

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,14 @@
 export default function modeChange(){
     const light=document.querySelector('.light');
     const dark=document.querySelector('.dark');
+    const option=document.querySelector('.option')
     dark.addEventListener('click',function(){
         light.classList.remove('d-none');
         this.classList.add('d-none');
         document.body.classList.add('bgLight');
         document.body.classList.remove('bgDark');
+        option.classList.add('bg-white');
+        option.classList.remove('bgFilter');
     })
     
     light.addEventListener('click',function(){
@@ -15,5 +18,7 @@ export default function modeChange(){
         this.classList.add('d-none');
         document.body.classList.remove('bgLight');
         document.body.classList.add('bgDark');
+        option.classList.remove('bg-white');
+        option.classList.add('bgFilter');
     })
 }
