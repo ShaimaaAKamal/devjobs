73364087e29707dbe92b5c889c606d5225279e41
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-23 21:28:46 +0200

    update script to change check color

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,8 @@
 export default function modeChange(){
     const light=document.querySelector('.light');
     const dark=document.querySelector('.dark');
-    const option=document.querySelector('.option')
+    const option=document.querySelector('.option');
+    const checkFull=document.querySelector('#checkFull');
     dark.addEventListener('click',function(){
         light.classList.remove('d-none');
         this.classList.add('d-none');
@@ -11,6 +12,7 @@ export default function modeChange(){
         document.body.classList.remove('bgDark');
         option.classList.add('bg-white');
         option.classList.remove('bgFilter');
+        checkFull.style.backgroundColor='#E7E7E9'
     })
     
     light.addEventListener('click',function(){
@@ -20,5 +22,6 @@ export default function modeChange(){
         document.body.classList.add('bgDark');
         option.classList.remove('bg-white');
         option.classList.add('bgFilter');
+        checkFull.style.backgroundColor='#303744'
     })
 }
