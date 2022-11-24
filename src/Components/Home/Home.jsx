bbf073614117c47a9bc5173151caac87f4085afb
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 20:40:53 +0200

    add modeChange to both to presist mode between navigation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Option from '../OptionsComponent/Option';
 import Jobs from '../JobsComponent/Jobs';
+import modeChange from '../../js/script.js';
 
-export default function Home({jobs}) {
+
+export default function Home({jobs,mode,setMode}) {
+    useEffect(() => {
+    localStorage.setItem('page','home');
+    modeChange(mode,setMode);
+},[]);
   return (
     <div className='parent'>
       <Option/>
--- src/Components/JobDetailsComponent/JobDetails.jsx
+++ src/Components/JobDetailsComponent/JobDetails.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom';
 import JobHeader from '../JobHeader/JobHeader';
 import JobInfo from '../JobComponent/JobInfo';
+import modeChange from '../../js/script.js';
 
-export default function JobDetails() {
+export default function JobDetails({mode,setMode}) {
   const location=useLocation()
   const jobDetails=location.state.details;
-  const logo=location.state.logo
+  const logo=location.state.logo;
+  useEffect(() => {
+        localStorage.setItem('page','job');
+        modeChange(mode,setMode);
+    },[]);
   return (
     <>
         <div className='parent'>
           <JobHeader job={jobDetails} logo={logo}/>
           <JobInfo job={jobDetails}/>   
         </div>
-        <footer className='bg-white shadow py-3'>
+        <footer className='bg-white shadow py-3 lightSite'>
             <div className='mx-auto JobheaderWidth px-5 d-flex justify-content-between align-items-center'>
                 <div className='ms-3 d-none d-md-block'>
                       <h6>{jobDetails.position}</h6>
