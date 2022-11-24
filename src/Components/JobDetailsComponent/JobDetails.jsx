f1fbd476e68bbeadb70b8560117c04bee60d1ae2
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 12:40:05 +0200

    receive job details using uselocation hook

--- src/Components/JobDetailsComponent/JobDetails.jsx
+++ src/Components/JobDetailsComponent/JobDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
-export default function JobDetails({details}) {
+export default function JobDetails({id}) {
+  const location=useLocation()
+  const jobDetails=location.state.details;
   return (
     <div>JobDetails</div>
   )
