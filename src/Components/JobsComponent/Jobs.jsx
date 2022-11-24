a919c4b1973c346f5e05866be2ed6576569bf450
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 11:56:51 +0200

    Jobs component implemntation

--- src/Components/JobsComponent/Jobs.jsx
+++ src/Components/JobsComponent/Jobs.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import JobCard from '../JobCardComponent/JobCard';
 
-export default function Jobs() {
+export default function Jobs({jobs}) {
   return (
-    <div>Jobs</div>
+    <div className='container py-5'>
+      <div className="row g-4"> 
+      
+         {
+          jobs.map((job,index)=> 
+          <div className="col-lg-4 col-md-6" key={index}>
+             <JobCard job={job}/>
+          </div>
+          )
+         }
+      </div>
+    </div>
   )
 }
