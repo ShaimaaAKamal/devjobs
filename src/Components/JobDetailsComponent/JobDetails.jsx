661a46a11df3e3fcac57e1e0e48285b74c2ccf64
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 15:33:25 +0200

    add page footer

--- src/Components/JobDetailsComponent/JobDetails.jsx
+++ src/Components/JobDetailsComponent/JobDetails.jsx
@@ -7,9 +7,20 @@ export default function JobDetails() {
   const location=useLocation()
   const jobDetails=location.state.details;
   return (
-    <div className='parent'>
-       <JobHeader job={jobDetails}/>
-       <JobInfo job={jobDetails}/>   
-    </div>
+    <>
+        <div className='parent'>
+          <JobHeader job={jobDetails}/>
+          <JobInfo job={jobDetails}/>   
+        </div>
+        <footer className='bg-white shadow py-3'>
+            <div className='mx-auto JobheaderWidth px-5 d-flex justify-content-between align-items-center'>
+                <div className='ms-3 d-none d-md-block'>
+                      <h6>{jobDetails.position}</h6>
+                      <span className='textGray fw-light small'>So Digital Inc.</span>
+                </div>
+                <a href={jobDetails.apply} className='btn ApplyBtn mainBtn px-4 '>Apply Now</a>
+            </div>
+        </footer>
+    </>
   )
 }
