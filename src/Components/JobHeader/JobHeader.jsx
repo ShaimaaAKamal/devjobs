4ab07c4e53ffc0e3b1f4a87a27a83c6e88cbc965
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 14:32:38 +0200

    implement Job Header Component

--- src/Components/JobHeader/JobHeader.jsx
+++ src/Components/JobHeader/JobHeader.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default function JobHeader() {
+export default function JobHeader({job}) {
+    const [logo,setLogo]=useState(null);
+    useEffect(()=>{
+       async function getLogo(){
+        const logImage=await import(`./../../assets/logos/scoot.svg`);
+        setLogo(logImage.default)
+       }
+       getLogo();
+    },[])
+
   return (
-    <div>JobHeader</div>
+    <div className='mx-auto JobheaderWidth'>
+        <div className='row bg-white gx-0 rounded-1 '>
+                <div className='col-md-2 col-lg-2 d-none d-md-block'>
+                <div className='w-100 h-100 d-flex justify-content-center align-items-center' style={{backgroundColor:`${job.logoBackground}`}}>
+                    <img src={logo} alt={job.company} />
+                </div>
+                </div>
+                <div className='col-md-10 col-lg-10 px-md-3 py-4'>
+                       <div className='position-relative'>
+                            <span style={{backgroundColor:`${job.logoBackground}`}} className='logoMove d-inline-block d-md-none p-2 py-3 rounded-2 position-absolute'>
+                                <img src={logo} alt={job.company}/>
+                            </span>
+                            <div className='d-flex flex-column flex-md-row justify-content-md-between align-items-center pt-4 pt-md-0 '>
+                                        <div className='mb-4 mb-md-0'>
+                                            <h5 className='mb-0 pb-0'>{job.company}</h5>
+                                            <span className='text-muted fs-7'>{`${job.company.toLowerCase()}.com`}</span>
+                                        </div>
+                                        <a href={job.website} className='lightBtn small text-decoration-none px-3 py-2 rounded-1' >Company Site</a>
+                                </div>
+                       </div>
+                </div>
+        </div>
+    </div>
   )
 }
+
