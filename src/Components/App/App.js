23ce0f235ecda945a17a5d9c8b48a18a1dd524f2
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 20:43:23 +0200

    move changemode from here to components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,14 +7,15 @@ import { useEffect, useState } from 'react';
 import modeChange from '../../js/script.js';
 import data from '../../data.json'
 function App() {
-  let [jobs,setJobs]=useState(data)
-  useEffect(() => {
-    modeChange();
-},[]);
+  let [jobs,setJobs]=useState(data);
+  let [mode,setMode]=useState('light');
+//   useEffect(() => {
+//     modeChange(mode,setMode);
+// },[]);
   const routes=createBrowserRouter([{
     path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
-      {index:true,element:<Home jobs={jobs}/>},
-      {path:'job/:id',element:<JobDetails/>}
+      {index:true,element:<Home jobs={jobs} mode={mode} setMode={setMode}/>},
+      {path:'job/:id',element:<JobDetails mode={mode} setMode={setMode}/>}
     ]
   }])
   return (
