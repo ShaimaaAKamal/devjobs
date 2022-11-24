8c0ac0135792a12f3b50fb370583a167346e7754
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 12:23:14 +0200

    add show job feature by navigation to job route

--- src/Components/JobCardComponent/JobCard.jsx
+++ src/Components/JobCardComponent/JobCard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 export default function JobCard({job}) {
+  const navigate=useNavigate()
   const [companyLogo,setCompanyLogo]=useState(null);
   useEffect(()=>{
     async function getLogo(){
@@ -11,22 +13,21 @@ export default function JobCard({job}) {
      getLogo();
   },[])
 
-  // const showJob=(jobId)=>{
-  //   console.log(jobId);
-  // }
+  const showJob=(jobId)=>{
+    navigate(`/job/${jobId}`);
+  }
   return (
-     <div>
-       <span style={{backgroundColor:`${job.logoBackground}`}} className='d-inline-block p-2 py-3 rounded-2'>
+     <div className='position-relative '>
+       <span style={{backgroundColor:`${job.logoBackground}`}} className='d-inline-block p-2 py-3 rounded-2 position-absolute translate-middle-y ms-5'>
             <img src={companyLogo} alt={job.company}/>
        </span>
-        <div className='bg-white px-3 py-4 rounded-3'>
+        <div className='bg-white  p-4 rounded-3'>
             <div className='fs-7 text-muted mt-3'>
               <span>{job.postedAt}</span>
               <span className='px-2'>.</span>
               <span>{job.contract}</span>
             </div>
-            <h6 className='py-2 mb-1'>{job.position}</h6>
-            {/* <h6 className='py-2 mb-1' onClick={()=>showJob(job.id)}>{job.position}</h6> */}
+            <h6 className='py-2 mb-1 jobPosition' onClick={()=>showJob(job.id)}>{job.position}</h6>
             <div className='fs-7 text-muted'><span>{job.company}</span></div>
             <br/><br/>
             <div className='fs-7 textSite fw-semibold'><span>{job.location}</span></div>
