388e16a4d5cde206c9df50cdc31938313a560a6c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 11:55:35 +0200

    add jobs states to app component to use it to display jobs

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,16 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from '../Layout/Layout';
 import Home from '../Home/Home'
 import NotFound from "../NotFound/NotFound";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import modeChange from '../../js/script.js';
-
+import data from '../../data.json'
 function App() {
+  let [jobs,setJobs]=useState(data)
   useEffect(() => {
     modeChange();
 },[]);
   const routes=createBrowserRouter([{
     path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
-      {index:true,element:<Home/>}
+      {index:true,element:<Home jobs={jobs}/>}
     ]
   }])
   return (
