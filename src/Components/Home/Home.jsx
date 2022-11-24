423b15a248c16f614406181d639cf5dc7ac63d50
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 11:56:12 +0200

    add jobs component to home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Option from '../OptionsComponent/Option';
+import Jobs from '../JobsComponent/Jobs';
 
-export default function Home() {
+export default function Home({jobs}) {
   return (
-    <>
+    <div className='parent'>
       <Option/>
-    </>
+      <Jobs jobs={jobs}/>
+    </div>
   )
 }
