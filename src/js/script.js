220f1f750d0efa7205e57aee081b792f3147768b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-24 20:39:19 +0200

    update modeChange logic

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,46 @@
 
 
-export default function modeChange(){
+export default function modeChange(mode,setMode){
     const light=document.querySelector('.light');
     const dark=document.querySelector('.dark');
     const Elements=document.querySelectorAll('.lightSite');
     const checkFull=document.querySelector('#checkFull');
-    dark.addEventListener('click',function(){
+    const page=localStorage.getItem('page')
+    const setLight=()=>{
+        setMode('light');
         light.classList.remove('d-none');
-        this.classList.add('d-none');
+        dark.classList.add('d-none');
         document.body.classList.add('bgLight');
         document.body.classList.remove('bgDark');
         Elements.forEach(element => {
             element.classList.add('bg-white');
             element.classList.remove('bgFilter');
         });
-        checkFull.style.backgroundColor='#E7E7E9'
-    })
-    
-    light.addEventListener('click',function(){
+        if(page === 'home') checkFull.style.backgroundColor='#E7E7E9'
+    }
+
+    const setDark=()=>{
+        setMode('dark');
         dark.classList.remove('d-none');
-        this.classList.add('d-none');
+        light.classList.add('d-none');
         document.body.classList.remove('bgLight');
         document.body.classList.add('bgDark');
         Elements.forEach(element => {
             element.classList.remove('bg-white');
             element.classList.add('bgFilter');
         });
-        // option.classList.remove('bg-white');
-        // option.classList.add('bgFilter');
-        checkFull.style.backgroundColor='#303744'
+        if(page === 'home')  checkFull.style.backgroundColor='#303744'
+
+    }
+
+    (mode === 'light')?setLight():setDark();
+
+    dark.addEventListener('click',function(){
+     setLight();
     })
+    
+    light.addEventListener('click',function(){
+       setDark();
+    })
+
 }
