41c5418f119e82f8d6c9c3a9b7becacfddc7807f
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-25 18:58:35 +0200

    save mode in local storage to presist state between refresh

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,17 +3,14 @@ import Layout from '../Layout/Layout';
 import Home from '../Home/Home'
 import NotFound from "../NotFound/NotFound";
 import JobDetails from "../JobDetailsComponent/JobDetails";
-import { useEffect, useState } from 'react';
-import modeChange from '../../js/script.js';
+import {useState } from 'react';
 import data from '../../data.json'
 function App() {
+  const initMode=localStorage.getItem('mode')?localStorage.getItem('mode'):'light'
   let [jobs,setJobs]=useState(data);
-  let [mode,setMode]=useState('light');
-//   useEffect(() => {
-//     modeChange(mode,setMode);
-// },[]);
+  let [mode,setMode]=useState(initMode);
   const routes=createBrowserRouter([{
-    path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
+    path:'',element:<Layout/>,errorElement:<NotFound jobs={jobs} mode={mode} setMode={setMode}/>,children:[
       {index:true,element:<Home jobs={jobs} mode={mode} setMode={setMode}/>},
       {path:'job/:id',element:<JobDetails mode={mode} setMode={setMode}/>}
     ]
