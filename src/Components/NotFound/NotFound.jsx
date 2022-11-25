5b53fce5d7a77f329b104bd9b93f4e73a037599b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-25 18:57:44 +0200

    implement not foound component

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import NotFoundImage from '../../assets/404.png'
+import { Link } from 'react-router-dom';
 
-export default function NotFound() {
+export default function NotFound({mode}) {
+  const handleCick=()=>{
+    document.body.style.backgroundColor=(mode=== 'light')?'#F4F6F8':'#121721';
+  }
+  useEffect(()=>{
+    document.body.style.backgroundColor='#262A37';
+  },[])
   return (
-    <div>NotFound</div>
+    <div className='vh-100 d-flex align-items-center justify-content-center position-relative'>
+    <Link to='/' className='text-decoration-none textGray fw-bold ps-2 errorBack position-absolute' onClick={handleCick}><i className="fa-solid fa-chevron-left me-2"></i><span>Back to Jobs</span></Link>
+    <img src={NotFoundImage} alt="404 Image" className='w-100' />
+   </div>
   )
 }
